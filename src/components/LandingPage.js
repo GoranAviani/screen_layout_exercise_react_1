49f58b32fd18d25a5516f8f11944f410c2d62ed1
GoranAviani <goranaviani@gmail.com>
2022-05-16 20:04:32 +0200

    screen_layout_exercise_react_1

---
+++ src/components/LandingPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const LandingPage = () => {
+
+    return (
+        <div>
+            <div class="leftSide">
+                left side
+            </div>
+            <button class="centerButton">Center button</button>
+            <div class="rightSide">
+                right side
+                <button class="topLeftButton">top left</button>
+            </div>
+
+        </div>
+    )
+}
